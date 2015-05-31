10e982f56af1305b709e0409feb83382afbf8df9
Gon Zifroni <gon@neo.com>
2015-05-31 10:19:36 -0700

    Added `mount_ebs_store` recipe to symlink the `repos` directory to an auto-mounting persistent EBS volume.

---
+++ mount_ebs_store/recipes/default.rb
@@ -0,0 +1,18 @@
+mount_point = node['ebs']['raids']['/dev/md0']['mount_point'] rescue nil
+
+if mount_point
+  node[:deploy].each do |application, deploy|
+    unless Dir.exist? "#{mount_point}/repos"
+      directory "#{mount_point}/repos" do
+        owner deploy[:user]
+        group deploy[:group]
+        mode 0770
+        recursive true
+      end
+    end
+
+    link "/srv/www/#{application}/current/repos" do
+      to "#{mount_point}/repos"
+    end
+  end
+end
