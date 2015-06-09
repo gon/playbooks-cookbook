118b31de0325a730423287ca585d09e32c5c06cd
Gon Zifroni <gon@zifroni.com>
2015-06-08 22:01:03 -0700

    Fixed Opsworks app name.

--- hydrate/recipes/default.rb
+++ hydrate/recipes/default.rb
@@ -1,6 +1,6 @@
 execute "rake storage:production:hydrate" do
-  cwd "#{node[:deploy]['myapp'][:deploy_to]}/current"
+  cwd "#{node[:deploy]['playbooks_api'][:deploy_to]}/current"
   user "deploy"
-  environment  ({"RAILS_ENV" => node[:deploy]['myapp'][:rails_env]})
+  environment  ({"RAILS_ENV" => node[:deploy]['playbooks_api'][:rails_env]})
   command "/usr/local/bin/bundle exec rake storage:production:hydrate"
 end
